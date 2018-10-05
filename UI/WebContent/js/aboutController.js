30396bacee9428976187195a46516de7af3ed338
Abhishek Yadav -X (abhiyada - ARICENT TECHNOLOGIES MAURITIUS LIMITED at Cisco) <abhiyada@partnet.cisco.com>
2018-10-05 23:30:50 +0530

    removed unused files

--- UI/WebContent/js/aboutController.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('myApp')
-app.controller('AboutController', function($scope, $rootScope, $stateParams, $state, $http,$location, $cookies, cfg) {
-	$scope.username = $cookies.username;
-	console.log("username---- "+ $scope.username);
-	  if(!$scope.username){
-		  $location.path('/login/');
-	  }
-	
-});
